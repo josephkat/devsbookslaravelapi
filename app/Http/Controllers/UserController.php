b8585aa38774d64522a6dba9d3885df00bbe5482
josephkat <zecakat@gmail.com>
2022-01-31 18:31:08 -0300

    adding update function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,81 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Facades\Auth;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    private $loggerdUser;
+
+    public function __construct() {
+        $this->middleware('auth:api');
+        $this->loggedUser = auth()->user();
+    }
+
+    public function update(Request $request) {
+        $array = ['error'=>''];
+
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $birthdate = $reques->input('birthdate');
+        $city = $request->input('city');
+        $work = $request->input('work');
+        $password = $request->input('password');
+        $password_confirm = $request->input('password_confirm');
+
+        $user = User::find($this->loggedUser['id']);
+
+        //NAME
+        if($name) {
+            $user->name = $name;
+        }
+
+        // E-MAIL
+        if($email) {
+            if($email != $user->email) {
+                $emailExists = User::where('email', $email)->count();
+                if($emailExists === 0) {
+                    $user->email = $email;
+                } else {
+                    $array['error'] = 'E-mail já existe!';
+                    return $array;
+                }
+            }
+        }
+
+        //BIRTHDATE
+        if($birthdate) {
+            if(strtotime($birthdate) === false) {
+                $array['error'] = 'Data de nascimento inválida';
+                return $array;
+            }
+            $user->birthdate = $birthdate;
+        }
+
+        //CITY
+        if($city) {
+            $user->city = $city;
+        }
+
+        //WORK
+        if($work) {
+            $user->work = $work;
+        }
+
+        //PASSWORD
+        if($password && $password_confirm) {
+            if($password === $password_confirm) {
+                $hash = password_hash($password, PASSWORD_DEFAULT);
+                $user->password = $hash;
+            } else {
+                $array['error'] = 'As senhas não batem.';
+                return $array;
+            }
+        }
+
+        $user->save();
+
+        return $array;
+    }
 }

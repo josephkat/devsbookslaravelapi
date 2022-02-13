5d1146cfe5401b61d266851a5b180f86fef568ed
josephkat <zecakat@gmail.com>
2022-02-12 23:32:43 -0300

    adding read function in FeedController

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 use App\Models\Post;
+use App\Models\UserRelation;
+use App\Models\PostComment;
+use App\Models\User;
+
 use Image;
 
 class FeedController extends Controller
@@ -17,9 +21,35 @@ class FeedController extends Controller
         $this->loggedUser = auth()->user();
     }
 
-    public function read() {}
+    public function userFeed(Request $request, $id = false) {
+        $array = ['error'=>''];
 
-    public function userFeed() {}
+        if($id == false) {
+            $id = $this->loggedUser['id'];
+        }
+
+        $page = intval($request->input('page'));
+        $perPage = 2;
+
+        // 1. Pegar os posts dessa galera ORDENADO PELA DATA
+        $postList = Post::where('id_user', $id)
+        ->orderBy('created_at', desc)
+        ->offset($page * $perPage)
+        ->limit($perPage)
+        ->get();
+
+        $total = Post::where('id_user', $id)->count();
+        $pageCount = ceil($total / $perPage);
+
+        // 2. Preencher as informações adicionais
+        $posts = $this->_postListToObject($postList, $this->loggedUser['id']);
+
+        $array['posts'] = [];
+        $array['pageCount'] = $pageCount;
+        $array['currentPage'] = $page;
+
+        return $array;
+    }
 
     public function create(Request $request) {
         $array = ['error' => ''];
@@ -86,4 +116,79 @@ class FeedController extends Controller
 
         return $array;
     }
+
+    public function read(Request $request) {
+        //GET api/feed (page)
+        $array =  ['error' => ''];
+
+        $page = intval($request->input('page'));
+        $perPage = 2;
+
+        // 1. Pegar a lista de usuarios que EU sigo (incluindo EU mesmo)
+        $users = [];
+        $userList = UserRelation::where('user_from', $this->loggedUser['id'])->get();
+        foreach($userList as $userItem) {
+            $users[] = $userItem['user_to'];
+        }
+        $users[] = $this->loggedUser['id'];
+
+        // 2. Pegar os posts dessa galera ORDENADO PELA DATA
+        $postList = Post::wherIn('id_user', $users)
+        ->orderBy('created_at', desc)
+        ->offset($page * $perPage)
+        ->limit($perPage)
+        ->get();
+
+        $total = Post::whereIn('id_user', $users)->count();
+        $pageCount = ceil($total / $perPage);
+
+        // 3. Preencher as informações adicionais
+        $posts = $this->_postListToObject($postList, $this->loggedUser['id']);
+
+        $array['posts'] = [];
+        $array['pageCount'] = $pageCount;
+        $array['currentPage'] = $page;
+
+        return $array;
+    }
+
+    private function _postListToObject($postList, $loggedId) {
+        foreach($postList as $postKey => $postItem) {
+
+            // verificar se o post é meu
+            if($postItem['id_user'] == $loggedId) {
+                $postList[$postKey]['mine'] = true; 
+            } else {
+                $postList[$postKey]['mine'] = false;
+            }
+
+            // Preencher informações de usuário
+            $userInfo = User::find($postItem['id_user']);
+            $userInfo['avatar'] = url('media/avatars/'.$userInfo['avatar']);
+            $userInfo['cover'] = url('media/avatars/'.$userInfo['cover']);
+            $postList[$postKey]['user'] = $userInfo;
+
+            // Preencher informações de LIKE
+            $likes = PostLike::where('id_post', $postItem['id'])->count();
+            $postList[$postKey]['likeCount'] = $likes;
+            
+            $isLiked = PostLike::where('id_post', $postItem['id'])
+            ->where('id_user', $loggedId)
+            ->count();
+            $postList[$postKey]['liked'] = ($isLiked > 0) ? true : false;
+
+            // Preencher informações de COMMENTS
+            $comments = PostComment::where('id_post', $postItem['id'])->get();
+            foreach($comments as $commentKey => $comment) {
+                $user = User::find($comment['id_user']);
+                $user['avatar'] = url('media/avatars/'.$user['avatar']);
+                $user['cover'] = url('media/covers/'.$user['cover']);
+                $comments[$commentKey]['user'] = $user;
+            }
+            $postList[$postKey]['comments'] = $comments;
+        }
+
+        return $postList;
+    }
+
 }
